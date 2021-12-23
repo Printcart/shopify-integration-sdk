ac8a71945929ddbfa7f66fff39717e18615de004
Hiep Vu <HiepVu511@users.noreply.github.com>
2021-12-23 12:09:09 +0700

    init

---
+++ main.js
@@ -0,0 +1,92 @@
+(function () {
+  var shopifyIntegration = {
+    sdkUrl:
+      "https://d1jkuyr6ycnsww.cloudfront.net/design-tool/1.0.0/main.js",
+
+    apiUrl: "http://localhost:8001/api/v1/integration/shopify/products/",
+
+    token: "",
+
+    init: function () {
+      this.token = this.getUnauthToken();
+
+      var shopifyProductID = this.getShopifyProductId();
+
+      // this.getPrintcartProductByShopifyId(productID);
+
+      this.getPrintcartProductByShopifyId(shopifyProductID)
+        .then((res) => {
+          var printcartProductId = res.data.id;
+
+          this.addSdkToPage(printcartProductId);
+        })
+        .catch((err) => console.warn(err.message));
+    },
+
+    addSdkToPage: function (productId) {
+      if (!productId) {
+        throw new Error("Missing Printcart Product ID");
+      }
+
+      var script, scriptContent;
+      scriptContent = document.createElement("script");
+      scriptContent.type = "text/javascript";
+      scriptContent.async = true;
+      scriptContent.id = "printcart-design-tool-sdk";
+      scriptContent.setAttribute("data-unauthToken", this.token);
+      scriptContent.setAttribute("data-productId", productId);
+      scriptContent.src = this.sdkUrl;
+      script = document.getElementsByTagName("script")[0];
+      return script.parentNode.insertBefore(scriptContent, script);
+    },
+
+    getUnauthToken: function () {
+      var token = null;
+
+      var urlSearchParams = new URLSearchParams(
+        document.currentScript.src.split("?")[1]
+      );
+      var params = Object.fromEntries(urlSearchParams.entries());
+
+      if (params.shopT) {
+        token = params.shopT;
+      }
+
+      return token;
+    },
+
+    getShopifyProductId: function () {
+      var jsonElList = Array.from(document.querySelectorAll("script"));
+      var productJsonEl = jsonElList.find(
+        (jsonEl) => jsonEl.id && jsonEl.id.startsWith("ProductJson-")
+      );
+
+      // console.log(jsonElList)
+      if (productJsonEl) {
+        productId = JSON.parse(productJsonEl.innerHTML).id;
+      }
+
+      return productId;
+    },
+
+    getPrintcartProductByShopifyId: function (shopifyId) {
+      if (!shopifyId) {
+        throw new Error("Missing Shopify ID");
+      }
+
+      var url = this.apiUrl + shopifyId;
+      var authStr = "Bearer " + this.token;
+
+      // TODO: Add fetch polyfill
+      return fetch(url, {
+        headers: {
+          Authorization: authStr,
+        },
+      }).then((res) => res.json());
+    },
+  };
+
+  shopifyIntegration.init();
+
+  window["PrintcartShopifySDK"] = shopifyIntegration;
+})();
