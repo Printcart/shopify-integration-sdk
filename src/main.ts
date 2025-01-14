3ca0cbba8e094420c8923560a198ab14e2c65be0
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2025-01-14 16:22:09 +0700

    v2.3.31

--- src/main.ts
+++ src/main.ts
@@ -174,6 +174,9 @@ class PrintcartDesignerShopify {
       const isUploadEnabled = res.data.enable_upload;
       const isQuotationRequest = res.data.enable_request_quote;
 
+      // Language
+      this.#language();
+
       if (isDesignEnabled) {
         this.#designerInstance = new PrintcartDesigner({
           token: this.token,
@@ -208,8 +211,6 @@ class PrintcartDesignerShopify {
       this.#openSelectModal();
       this.#registerCloseModal();
       this.#modalTrap();
-      // Language
-      this.#language();
     });
   }
 
